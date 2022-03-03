78ce6d495864a44fbda606a5f12c965313349a6f
Ananya Agarwal <80957726+ananyaa01@users.noreply.github.com>
2022-03-03 10:42:18 +0530

    double tap zoom feature added

--- Pinch/Screens/ContentView.swift
+++ Pinch/Screens/ContentView.swift
@@ -8,15 +8,64 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    //MARK: Property
+    @State private var isAnimating: Bool = false
+    @State private var imageScale : CGFloat = 1
+    
+    
+    //MARK: Body
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            ZStack{
+                //MARK: PAGE IMAGE
+                Image("magazine-front-cover")
+                    .resizable()
+                    .aspectRatio( contentMode: .fit)
+                    .cornerRadius(10)
+                    .padding()
+                    .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+                    .opacity(isAnimating ? 1 : 0)
+                    .animation(.linear(duration: 1), value: isAnimating)
+                    .scaleEffect(imageScale)
+                //MARK: PROPERTY 1
+                    .onTapGesture (count: 2 , perform:{
+                        if imageScale == 1 {
+                            withAnimation(.spring()){
+                                imageScale = 5
+                                
+                            }
+                        }
+                        else{
+                            withAnimation(.spring()){
+                                imageScale = 1
+
+                            }
+                        }
+                            
+                        }
+                    )
+            
+                
+                
+        }
+            .navigationTitle("Pinch and Zoom")
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear(){
+                isAnimating = true
+            }
+        
+        
+        
     }
+        .navigationViewStyle(.stack)
+}
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-            .preferredColorScheme(.dark)
+      .preferredColorScheme(.dark)
     }
 }
